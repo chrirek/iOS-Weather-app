1faae72e5bbd91f14b8aaf7d24473aa6d4b34988
Christian Rekdal <rekdal.christian@gmail.com>
2018-06-21 14:43:15 +0200

    Fixed error in Foundation

--- Stormy/DarkSkyAPIClient.swift
+++ Stormy/DarkSkyAPIClient.swift
@@ -10,34 +10,34 @@ import Foundation
 
 class DarkSkyAPIClient {
     fileprivate let darkSkyApiKey = "b853321513ee430a225b12b5f02b8dd7"
-    
+
     lazy var baseUrl: URL = {
         return URL(string: "https://api.darksky.net/forecast/\(self.darkSkyApiKey)/")!
     }()
-    
+
     let decoder = JSONDecoder()
     let session: URLSession
-    
+
     init(configuration: URLSessionConfiguration) {
         self.session = URLSession(configuration: configuration)
     }
-    
+
     convenience init() {
         self.init(configuration: .default)
     }
-    
+
     typealias WeatherCompletionHandler = (Weather?, Error?) -> Void
     typealias CurrentWeatherCompletionHandler = (CurrentWeather?, Error?) -> Void
-    
+
     private func getWeather(at coordinate: Coordinate, completionHandler completion: @escaping WeatherCompletionHandler) {
-        
+
         guard let url = URL(string: coordinate.description, relativeTo: baseUrl) else {
             completion(nil, DarkSkyError.invalidUrl)
             return
         }
-        
+
         let request = URLRequest(url: url)
-        
+
         let task = session.dataTask(with: request) {data, response, error in
             DispatchQueue.main.async {
                 if let data = data {
@@ -60,38 +60,13 @@ class DarkSkyAPIClient {
                 }
             }
         }
-        
+
         task.resume()
     }
-    
+
     func getCurrentWeather(at coordinate: Coordinate, completionHandler completion: @escaping CurrentWeatherCompletionHandler) {
         getWeather(at: coordinate) { weather, error in
             completion(weather?.currently, error)
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
